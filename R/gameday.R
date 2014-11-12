81b5512ac1f0637ccc88233d732b2d72dac16ce8
Kurtis Stewart <kurtisstewart1948@gmail.com>
2014-11-12 10:36:26 -0800

    first commit

---
+++ R/gameday.R
@@ -0,0 +1,11 @@
+#' My first R package: gameday
+#'
+#' This is so much fun
+#'
+#' I look forward to writing really neat packages
+#' so that I can have all my helper functions in one place
+#' and maybe even share them.
+#'
+#' @docType package
+#' @name gameday
+NULL
